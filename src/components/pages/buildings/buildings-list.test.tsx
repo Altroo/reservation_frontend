39a6f4e273f8e3c34437db9a9736c4002270256c
Altroo <scorpion.yooy@gmail.com>
2026-03-31 11:43:31 +0100

    test: remove unused imports in buildings list test file

--- src/components/pages/buildings/buildings-list.test.tsx
+++ src/components/pages/buildings/buildings-list.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, cleanup, fireEvent, act, waitFor } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 // Mock next/navigation
